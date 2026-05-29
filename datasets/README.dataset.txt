a3c92d4638fd2f86855e8d3e97f97e1c72966401
saiyam-bajpai <officialsb1009@gmail.com>
2026-05-29 20:45:09 +0530

    My part

---
+++ datasets/README.dataset.txt
@@ -0,0 +1,6 @@
+# Pothole Detection > 2024-12-08 8:11pm
+https://universe.roboflow.com/model-training-6ezyd/pothole-detection-9uyrx
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import os
-os.chdir(r"C:\Users\asus\Downloads\RoadWatch AI")
+os.chdir(r"C:\Users\asus\Downloads\Pothole Detection Model RoadWatch AI")
 
 from ultralytics import YOLO
 
-model_path = r"C:\Users\asus\Downloads\RoadWatch AI\outputs\best.pt"
-image_path = r"C:\Users\asus\Downloads\RoadWatch AI\testing\OIP.jpeg"
+model_path = r"C:\Users\asus\Downloads\Pothole Detection Model RoadWatch AI\outputs\best.pt"
+image_path = r"C:\Users\asus\Downloads\Pothole Detection Model RoadWatch AI\testing\WhatsApp Image 2026-05-29 at 1.50.04 AM.jpeg"
 
 model = YOLO(model_path)
 
 results = model.predict(
     source=image_path,
-    conf=0.45,
+    conf=0.25,
     save=True,
-    project=r"C:\Users\asus\Downloads\RoadWatch AI\runs",
+    project=r"C:\Users\asus\Downloads\Pothole Detection Model RoadWatch AI\runs",
     name="test_result"
 )
 
 print("Number of detections:", len(results[0].boxes))
-print(f"Result saved to: C:\\Users\\asus\\Downloads\\RoadWatch AI\\runs\\test_result\\")
+print(f"Result saved to: C:\\Users\\asus\\Downloads\\Pothole Detection Model RoadWatch AI\\runs\\test_result\\")
